68e9363d61de750d0c0767e59bca351f3de38a5f
Abhishek Yonghang <ayongh1@gamil.com>
2022-06-23 11:18:39 -0500

    fixed backend url

--- my-app/src/API/backendHost.js
+++ my-app/src/API/backendHost.js
@@ -1,1 +1,8 @@
-export const backend = "http://localhost:3001/api";
+let backendUrl = "";
+if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
+  backendUrl = "http://localhost:3001/api";
+} else {
+  backendUrl = "https://nac2022.herokuapp.com/api";
+}
+
+export const backend = backendUrl;
